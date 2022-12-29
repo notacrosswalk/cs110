1c015ab284a09fa004efd1ca00ac178ea68deeec
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2022-12-29 21:41:08 +0530

    Add and update files

--- assignment_4/5/xi.c
+++ assignment_4/5/xi.c
@@ -1,17 +1,17 @@
 /* 
-* 4
-* 33
-* 222
-* 1111
+*    0
+*   01
+*  012
+* 0123
 */
 
 #include <stdio.h>
 int main() {
     int n = 4; // Change the input here
-    int i = n;
-    while(i > 0) {
-        int j = 0;
-        while(j <= n - i) {
+    int i = 0;
+    while(i < n) {
+        int j = 1;
+        while(j <= i) {
             printf("%d", i);
             j++;
         }
