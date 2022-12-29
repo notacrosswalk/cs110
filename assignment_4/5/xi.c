ff38442e679ffa0f150c5f472b608d662db17ebc
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2022-12-29 21:44:44 +0530

    Update xi.c

--- assignment_4/5/xi.c
+++ assignment_4/5/xi.c
@@ -10,13 +10,16 @@ int main() {
     int n = 4; // Change the input here
     int i = 0;
     while(i < n) {
-        int j = 1;
+        int j = 1, k = n;
+        while(k > i+1) {
+            printf(" ");
+        }
         while(j <= i) {
             printf("%d", i);
             j++;
         }
         printf("\n");
-        i--;
+        i++;
     }
     return 0;
 }
