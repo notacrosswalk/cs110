d0093eff10102cdb0345306a312317fecdcca733
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-01-02 15:38:28 +0530

    Update 16.c

--- assignment_4/16.c
+++ assignment_4/16.c
@@ -1,14 +1,22 @@
 /* Write a program in C to print the prime numbers that are less 
 than a given value n. */
 
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-    int n = 5; // Change the input here
-    int f = 1;
-    for(int i = 1; i <= n; i++) {
-         f *= i;
+    int n = 14; // Change the input here
+    printf("The prime numbers less than %d are ", n);
+    for(int i = 2; i < n; i++) {
+        int condition = 1;
+        for(int j = i - 1; j >= sqrt(i); j--) {
+            if(i%j == 0) {
+                condition = 0;
+                break;
+            }
+        }
+        if(condition == 1)
+            printf("%d ", i);
     }
-    printf("%d is the factorial of %d\n", f, n);
     return 0;
 }
