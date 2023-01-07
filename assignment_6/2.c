83d2bbd00de5bdb8c57c811856acd3a0466c0aa5
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-01-07 09:31:55 +0000

    Add files

--- assignment_6/2.c
+++ assignment_6/2.c
@@ -6,13 +6,37 @@ function after the main function and declare the function inside the main functi
 #include <math.h>
 #include <stdio.h>
 
+
+int main() {
+
+  int number = 36; // Change the input here
+
+  int isPrime(int n); // Function declaration
+  
+  int result = isPrime(number);
+
+  if(result == 0)
+    printf("%d is not a prime number.\n", number);
+  else
+    printf("%d is a prime number.\n", number);
+  
+  return 0;
+}
+
 int isPrime(int n) {
+  
   int condition = 1;
-  for(int i = 2; i<= sqrt(n); i++) {
+  
+  for(int i = 2; i <= sqrt(n); i++) {
+  
     if(n%i == 0) {
-      c = 0;
+    
+      condition = 0;
+      
       break;
+    
     }
   }
-  return 
+  
+  return condition;
 }
