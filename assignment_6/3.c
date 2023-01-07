00d903e299706b1646e486d446cca829fa657f2d
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-01-07 09:35:27 +0000

    Add 3.c

---
+++ assignment_6/3.c
@@ -0,0 +1,5 @@
+/* Write a function in C to check whether a number is Armstrong or not. If it is an
+Armstrong number, the function should return 1. Otherwise, it should return 0.
+Write the main function and call the function from the main function. You need to
+define the function after the main function and declare the function outside (before)
+the main function. */
