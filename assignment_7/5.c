b41ac6e9a1476876497de161326ea9779cb67abc
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-02-01 06:24:07 +0000

    Update files

--- assignment_7/5.c
+++ assignment_7/5.c
@@ -2,18 +2,34 @@
 // elements in an array.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int numberOfDuplicates(int *arr, int size) {
-  int flag[size];
+  int *flag = (int *)calloc(size, sizeof(int));
+  int n = 0;
   for (int i = 0; i < size; i++) {
     if (!flag[i]) {
-      for (int j = 0; j < size; j++) {
-        flag[j] = a[i] == a[j];
-        flag[i] == flag[i] || flag[j];
+      int c = 0;
+      for (int j = i + 1; j < size; j++) {
+        if (arr[i] == arr[j] && !flag[j]) {
+          flag[j] = 1;
+          flag[i] = flag[j];
+          if (c++ == 0) n++;
+        }
       }
     }
   }
   return n;
 }
 
-int main() { return 0; }
+int main() {
+  int n;
+  printf("Enter the number of elements in the array:\n");
+  scanf("%d", &n);
+  int *a = (int *)malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  int n_duplicates = numberOfDuplicates(a, n);
+  printf("There are %d duplicates in the array.\n", n_duplicates);
+  return 0;
+}
