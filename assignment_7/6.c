428def2bca28c1b2124fda43bf9dbdcbd98cfaae
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-02-01 06:33:42 +0000

    Add 6.c

---
+++ assignment_7/6.c
@@ -0,0 +1,27 @@
+// Write a program in C to count the occurrences of odd numbers in an array.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int oddOccurrences(int *arr, int size);
+
+int main() {
+  int n;
+  printf("Enter the number of elements in the array:\n");
+  scanf("%d", &n);
+  int *a = (int *)malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  printf("There are %d occurrences of odd numbers in the array.\n",
+         oddOccurrences(a, n));
+  return 0;
+}
+
+int oddOccurrences(int *arr, int size) {
+  int n;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] % 2)
+      n++;
+  }
+  return n;
+}
