6b8b85950c92b26552db9fcdcde54a43069d3ef0
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-02-18 16:24:08 +0530

    Create 7.c

---
+++ assignment_8/7.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <limits.h>
+
+int max_sum(int a[], int size)
+{
+    int maximum=INT_MIN;
+
+    for(int i=0; i<size; i++)
+    {
+        int sum = 0;
+        for(int j=i; j<size; j++)
+        {
+            sum += a[j];
+            if(sum > maximum) maximum = sum;
+        }
+        for(int j=0; j<i; j++)
+        {
+            sum += a[j];
+            if(sum > maximum) maximum = sum;
+        }
+    }
+    return maximum;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the array size.\n");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter the array elements.\n");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("%d\n", max_sum(a, n));
+    return 0;
+}
