c98ec67de9fd876ba5d96dc40ba9e9c9cf2de6c8
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-03-14 13:25:30 +0530

    Update 1.c

--- assignment_12/1.c
+++ assignment_12/1.c
@@ -1,4 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t
+{
+    int data;
+    struct node_t *next;
+} Node_t, *Node;
+
+void printList(Node node)
+{
+    if(node == NULL) return;
+    printf("%d -> ", node->data);
+    printList(node->next);
+}
+
+void addFirst(Node *node, int data)
+{
+    Node temp = (Node) calloc(1, sizeof(Node_t));
+    temp->data = data;
+    temp->next = *node;
+    *node = temp;
+}
+
+void addLast(Node *node, int data)
+{
+    Node t = *node;
+    if(node == NULL)
+    {
+        addFirst(node, data);
+        return;
+    }
+    while(1)
+    {
+        if(t->next == NULL)
+        {
+            Node temp = (Node) calloc(1, sizeof(Node_t));
+            temp->data = data;
+            temp->next = NULL;
+            t->next = temp;
+            break;
+        }
+        t = t->next;
+    }
+}
+
+int removeFirst(Node *node)
+{
+    int value = *node->data;
+    Node pnode = *node;
+    *node = *node->next;
+    free(pnode);
+    return value;
+}
+
+int removeLast(Node *node)
+{
+    Node pnode = *node;
+    while(1)
+    {
+        if(pnode->next->next == NULL)
+        {
+            int value = pnode->next->data;
+            free(pnode->next);
+            pnode->next = NULL;
+            return data;
+        }
+    }
+}
+
+int addAt(Node *node, int data, int index)
+{
+    int n = 0;
+    Node newNode = (Node) calloc(1, sizeof(Node_t));
+    Node temp = *node;
+    while(temp->next)
+    {
+        if(n++ == index)
+        {
+
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
+
+}
 
 int main() {
   Node list = NULL;
